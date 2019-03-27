4731808cfdbc1fb98d95eaa7edaaf1783e5deabc
Bilal Ahmed <bilalahedim@gmail.com>
2019-03-27 11:37:42 +0530

    Noramal Navigation update mobile left

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
         </a>
       </div>
 
-      <div class="nav-menu">
+      <div class="nav-menu d-md-none">
         <div class="nav-menu__middle"></div>
       </div>
 
@@ -25,20 +25,24 @@
         <ul class="nav-menu__item">
           <li><a href="<?php echo get_site_url()?>">Home</a></li>
           <li><a href="<?php echo get_site_url().'/index.php/bath-tubs?cat=2'?>">Bath Tubs</a></li>
-          <li><a class="drop">Farm House Sinks</a>
-            <ul class="nav-menu__item__sub">
-              <li><a href="<?php echo get_site_url().'/index.php/farm-house-sinks/single-bowl?cat=4'?>">Single Bowl</a></li>
-              <li><a href="<?php echo get_site_url().'/index.php/farm-house-sinks/double-bowl?cat=5'?>">Double Bowl</a></li>
+          <li><a href="javascript:void(0)">Products</a>
+            <ul>
+              <li><a href="javascript:void(0)" class="drop"> Farm House Sinks</a>
+                <ul class="nav-menu__item__sub">
+                  <li><a href="<?php echo get_site_url().'/index.php/farm-house-sinks/single-bowl?cat=4'?>">Single Bowl</a></li>
+                  <li><a href="<?php echo get_site_url().'/index.php/farm-house-sinks/double-bowl?cat=5'?>">Double Bowl</a></li>
+                </ul>
+              </li>
+              <li><a href="javascript:void(0)" class="drop">Under Mount Sinks</a>
+                <ul class="nav-menu__item__sub">
+                  <li><a href="<?php echo get_site_url().'/index.php/under-mount-sinks/single-bowl?cat=7'?>">Single Bowl</a></li>
+                  <li><a href="<?php echo get_site_url().'/index.php/under-mount-sinks/double-bowl?cat=8'?>">Double Bowl</a></li>
+                </ul>
+              </li>
+              <li><a href="<?php echo get_site_url().'/index.php/basin-sinks?cat=9'?>">Basin Sinks</a></li>
+              <li><a href="<?php echo get_site_url().'/index.php/pedestal-sinks?cat=10'?>">Pedestal Sinks</a></li>
             </ul>
           </li>
-          <li><a class="drop">Under Mount Sinks</a>
-            <ul class="nav-menu__item__sub">
-              <li><a href="<?php echo get_site_url().'/index.php/under-mount-sinks/single-bowl?cat=7'?>">Single Bowl</a></li>
-              <li><a href="<?php echo get_site_url().'/index.php/under-mount-sinks/double-bowl?cat=8'?>">Double Bowl</a></li>
-            </ul>
-          </li>
-          <li><a href="<?php echo get_site_url().'/index.php/basin-sinks?cat=9'?>">Basin Sinks</a></li>
-          <li><a href="<?php echo get_site_url().'/index.php/pedestal-sinks?cat=10'?>">Pedestal Sinks</a></li>
           <li><a href="<?php echo get_site_url().'/index.php/about-us'?>">About</a></li>
           <li><a href="<?php echo get_site_url().'/index.php/contact'?>">Contact</a></li>
         </ul>
