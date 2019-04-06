4934756cf3671904a4c3accc085b15325fe9d1bb
Bilal Ahmed <bilalahedim@gmail.com>
2019-04-06 15:07:31 +0530

    Footer complete.

--- footer.php
+++ footer.php
@@ -1,92 +1,50 @@
 <footer class="footer">
-
-  <div class="footer__nav text-center">
+  <div class="container">
     <div class="row">
-      <div class="col-md-6">
-        <div class="row">
 
-          <div class="col-lg-6">
-            <div class="footer__nav__copyright footer__divider">
-              <div class="brand">
-                <div class="brand__graphic"></div>
-              </div>
-              <div class="brand__text">
-                <h6>&copy;All Rights Reserved</h6>
-                <h6>Rams International</h6>
-              </div>
-            </div>
+      <div class="col-md-4">
+        <div class="brand">
+          <div class="logo">
+            <a href="<?php echo get_site_url();?>">Rams International</a>
+            <p>&copy;Rams International</p>
           </div>
+        </div>
 
-          <div class="col-lg-6">
-            <div class="rams-pages footer__divider">
-              <div class="section-heading">
-                <h5>Rams Pages</h5>
-              </div>
-              <ul class="text-justify footer__align--one">
-                <li><a class="active" href="<?php echo get_site_url()?>">Home</a></li>
-                <li><a href="<?php echo get_site_url().'/index.php/about-us'?>">About</a></li>
-                <li><a href="<?php echo get_site_url().'/index.php/contact'?>">Contact</a></li>
-              </ul>
-            </div>
-          </div>
-
+        <div class="social">
+          <ul>
+            <li><a href="#">Facebook</a></li>
+            <li><a href="#">Linkedin</a></li>
+            <li><a href="#">Google+</a></li>
+            <li><a href="#">Twitter</a></li>
+          </ul>
         </div>
       </div>
-
-      <div class="col-md-6">
-        <div class="row">
-
-          <div class="col-lg-6">
-            <div class="product-pages footer__divider">
-              <div class="section-heading">
-                <h5>Products Pages</h5>
-              </div>
-              <ul class="text-justify footer__align--two">
-
-                <li><a href="<?php echo get_site_url().'/bath-tubs?cat=2'?>">Bath Tubs</a></li>
-                <li><a href="<?php echo get_site_url().'/index.php/farm-house-sinks?cat=3'?>">Farm House Sink</a>
-
-                  <ul>
-                    <li><a href="<?php echo get_site_url().'/index.php/farm-house-sinks/single-bowl?cat=4'?>">Single Bowl</a></li>
-                    <li><a href="<?php echo get_site_url().'/index.php/farm-house-sinks/double-bowl?cat=5'?>">Double Bowl</a></li>
-                  </ul>
-                </li>
-
-                <li><a href="<?php echo get_site_url().'/index.php/under-mount-sinks?cat=6'?>">Under Mount Sink</a>
-                  <ul>
-                    <li><a href="<?php echo get_site_url().'/index.php/under-mount-sinks/single-bowl?cat=7'?>">Single Bowl</a></li>
-                    <li><a href="<?php echo get_site_url().'/index.php/under-mount-sinks/double-bowl?cat=8'?>">Double Bowl</a></li>
-                  </ul>
-                </li>
-
-                <li><a href="<?php echo get_site_url().'/index.php/basin-sinks?cat=9'?>">Basin Sinks</a></li>
-                <li><a href="<?php echo get_site_url().'/index.php/pedestal-sinks?cat=10'?>">Pedistal Sinks</a></li>
-
-              </ul>
-            </div>
-          </div>
-
-          <div class="col-lg-6">
-            <div class="social-pages footer__divider">
-              <div class="section-heading">
-                <h5>Social Pages</h5>
-              </div>
-              <ul class="text-justify footer__align--three">
-                <li><a href="#">FB</a></li>
-                <li><a href="#">GP</a></li>
-                <li><a href="#">TW</a></li>
-                <li><a href="#">PT</a></li>
-              </ul>
-            </div>
-          </div>
-
-        </div>
+      
+      <div class="col-sm-4">
+        <ul>
+          <li><a href="<?php echo get_site_url().'/index.php/copper-kitchen-bathtubs/bath-tubs?category_nameegory_name=bath-tubs'?>">Bath Tubs</a></li>
+          <li><a href="<?php echo get_site_url().'/index.php/copper-kitchen-bathtubs/hoods?category_name=hoods'?>">Hoods</a></li>
+          <li><a href="<?php echo get_site_url().'/index.php/copper-kitchen-bathtubs/farm-house-sinks?category_name=farm-house-sinks'?>">Farm House Sinks</a></li>
+          <li><a href="<?php echo get_site_url().'/index.php/copper-kitchen-bathtubs/under-mount-sinks?category_name=under-mount-sinks'?>">Under Mount Sinks</a></li>
+          <li><a href="<?php echo get_site_url().'/index.php/copper-kitchen-bathtubs/basin?category_name=basin'?>">Basin</a></li>
+          <li><a href="<?php echo get_site_url().'/index.php/copper-kitchen-bathtubs/pedestal?category_name=pedestal'?>">Pedestal</a></li>
+          <li><a href="<?php echo get_site_url().'/index.php/copper-kitchen-bathtubs/tiles?category_name=tiles'?>">Tiles</a></li>
+        </ul>
       </div>
+
+      <div class="col-sm-4">
+        <ul>
+          <li><a href="<?php echo get_site_url().'/index.php/handicrafts/utensils-tableware?category_name=utensils-tableware'?>">Utensils & Tableware</a></li>
+          <li><a href="<?php echo get_site_url().'/index.php/handicrafts/wall-decor?category_name=wall-decor'?>">Wall Decor</a></li>
+          <li><a href="<?php echo get_site_url().'/index.php/handicrafts/furniture?category_name=furniture'?>">Furniture</a></li>
+          <li><a href="<?php echo get_site_url().'/index.php/handicrafts/lamps-lighting?category_name=lamps-lighting'?>">Lamps & Lighting</a></li>
+          <li><a href="<?php echo get_site_url().'/index.php/handicrafts/garden?category_name=garden'?>">Garden</a></li>
+        </ul>
+      </div>
+
 
     </div>
   </div>
-
-
 </footer>
 <?php
   wp_footer();
