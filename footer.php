e51a25819764eaa12dc2ff859de674f81e9b2833
Bilal Ahmed <bilalahmedim@gmail.com>
2021-03-31 12:29:29 +0530

    footer update.

--- footer.php
+++ footer.php
@@ -1,51 +1,93 @@
 <footer class="footer">
-  <div class="container">
-    <div class="row">
 
-      <div class="col-md-4">
-        <div class="brand">
-          <div class="logo">
-            <a href="<?php echo get_site_url();?>">Rams International</a>
-            <p>&copy;Rams International</p>
+    <div class="footer__nav">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="row">
+
+            <div class="col-lg-6">
+              <div class="footer__nav__copyright footer__divider">
+                <div class="brand">
+                  <div class="brand__graphic"></div>
+                </div>
+                <div class="brand__text text-center">
+                  <img src=<?php echo get_template_directory_uri()."/assets/logo.png"?> width="65" height="65" class="d-inline-block" alt="Rams International Logo">
+                  <h6>&copy;All Rights Reserved</h6>
+                  <h6>Rams International</h6>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-lg-6 ">
+              <div class="rams-pages footer__divider">
+                <div class="section-heading">
+                  <h5>Rams Pages</h5>
+                </div>
+                <ul class="text-justify footer__align--one">
+                  <li><a class="active" href="#">Home</a></li>
+                  <li><a href="#">About</a></li>
+                  <li><a href="#">Contact</a></li>
+                </ul>
+              </div>
+            </div>
+
           </div>
         </div>
 
-        <div class="social">
-          <ul>
-            <li><a href="#">Facebook</a></li>
-            <li><a href="#">Linkedin</a></li>
-            <li><a href="#">Google+</a></li>
-            <li><a href="#">Twitter</a></li>
-          </ul>
+        <div class="col-md-6">
+          <div class="row">
+
+            <div class="col-lg-6">
+              <div class="product-pages footer__divider">
+                <div class="section-heading">
+                  <h5>Products Pages</h5>
+                </div>
+                <ul class="text-justify footer__align--two">
+
+                  <li><a href="#">Bath Tubs</a></li>
+                  <li><a href="#">FarmHouse Sink</a>
+
+                    <ul>
+                      <li><a href="#">Single Bowl</a></li>
+                      <li><a href="#">Double Bowl</a></li>
+                    </ul>
+                  </li>
+
+                  <li><a href="#">Under Mount Sink</a>
+                    <ul>
+                      <li><a href="#">Single Bowl</a></li>
+                      <li><a href="#">Double Bowl</a></li>
+                    </ul>
+                  </li>
+
+                  <li><a href="#">Basin Sinks</a></li>
+                  <li><a href="#">Pedistal</a></li>
+
+                </ul>
+              </div>
+            </div>
+
+            <div class="col-lg-6">
+              <div class="social-pages footer__divider">
+                <div class="section-heading">
+                  <h5>Social Pages</h5>
+                </div> 
+                <ul class="text-justify footer__align--three">
+                  <li><a href="https://www.facebook.com/ramsinternational" class="social-icons align-middle"><span class="fb">&nbsp;</span>Facebook</a></li>
+                  <li><a href="https://www.linkedin.com/company/rams-international-india/about/" class="social-icons align-middle"><span class="li">&nbsp;</span>Linkedin</a></li>
+                  <li><a href="https://twitter.com/anwer_riyaz" class="social-icons align-middle"><span class="tw">&nbsp;</span>Twitter</a></li>
+                  <li><a href="#" class="social-icons align-middle"><span class="pt">&nbsp;</span>Pinterest</a></li>
+                </ul>
+              </div>
+            </div>
+
+          </div>
         </div>
-      </div>
-      
-      <div class="col-sm-4">
-        <ul>
-          <li><a href="<?php echo get_site_url().'/index.php/copper-kitchen-bathtubs/bath-tubs?category_name=bath-tubs'?>">Bath Tubs</a></li>
-          <li><a href="<?php echo get_site_url().'/index.php/copper-kitchen-bathtubs/hoods?category_name=hoods'?>">Hoods</a></li>
-          <li><a href="<?php echo get_site_url().'/index.php/copper-kitchen-bathtubs/farm-house-sinks?category_name=farm-house-sinks'?>">Farm House Sinks</a></li>
-          <li><a href="<?php echo get_site_url().'/index.php/copper-kitchen-bathtubs/under-mount-sinks?category_name=under-mount-sinks'?>">Under Mount Sinks</a></li>
-          <li><a href="<?php echo get_site_url().'/index.php/copper-kitchen-bathtubs/basin?category_name=basin'?>">Basin</a></li>
-          <li><a href="<?php echo get_site_url().'/index.php/copper-kitchen-bathtubs/pedestal?category_name=pedestal'?>">Pedestal</a></li>
-          <li><a href="<?php echo get_site_url().'/index.php/copper-kitchen-bathtubs/tiles?category_name=tiles'?>">Tiles</a></li>
-        </ul>
-      </div>
 
-      <div class="col-sm-4">
-        <ul>
-          <li><a href="<?php echo get_site_url().'/index.php/handicrafts/utensils-tableware?category_name=utensils-tableware'?>">Utensils & Tableware</a></li>
-          <li><a href="<?php echo get_site_url().'/index.php/handicrafts/wall-decor?category_name=wall-decor'?>">Wall Decor</a></li>
-          <li><a href="<?php echo get_site_url().'/index.php/handicrafts/furniture?category_name=furniture'?>">Furniture</a></li>
-          <li><a href="<?php echo get_site_url().'/index.php/handicrafts/lamps-lighting?category_name=lamps-lighting'?>">Lamps & Lighting</a></li>
-          <li><a href="<?php echo get_site_url().'/index.php/handicrafts/garden?category_name=garden'?>">Garden</a></li>
-        </ul>
       </div>
-
-
     </div>
-  </div>
-</footer>
+
+  </footer>
 <?php
   wp_footer();
 ?>
---
+++ old files/old.footer.php
@@ -0,0 +1,58 @@
+<footer class="footer">
+  <div class="container">
+    <div class="row">
+
+      <div class="col-md-4">
+        <div class="brand">
+          <div class="logo">
+            <a href="<?php echo get_site_url();?>">Rams International</a>
+            <p>&copy;Rams International</p>
+          </div>
+        </div>
+
+        <div class="social">
+          <ul>
+            <li><a href="#">Facebook</a></li>
+            <li><a href="#">Linkedin</a></li>
+            <li><a href="#">Google+</a></li>
+            <li><a href="#">Twitter</a></li>
+          </ul>
+        </div>
+      </div>
+      
+      <div class="col-sm-4">
+        <ul>
+          <li><a href="<?php echo get_site_url().'/index.php/copper-kitchen-bathtubs/bath-tubs?category_name=bath-tubs'?>">Bath Tubs</a></li>
+          <li><a href="<?php echo get_site_url().'/index.php/copper-kitchen-bathtubs/hoods?category_name=hoods'?>">Hoods</a></li>
+          <li><a href="<?php echo get_site_url().'/index.php/copper-kitchen-bathtubs/farm-house-sinks?category_name=farm-house-sinks'?>">Farm House Sinks</a></li>
+          <li><a href="<?php echo get_site_url().'/index.php/copper-kitchen-bathtubs/under-mount-sinks?category_name=under-mount-sinks'?>">Under Mount Sinks</a></li>
+          <li><a href="<?php echo get_site_url().'/index.php/copper-kitchen-bathtubs/basin?category_name=basin'?>">Basin</a></li>
+          <li><a href="<?php echo get_site_url().'/index.php/copper-kitchen-bathtubs/pedestal?category_name=pedestal'?>">Pedestal</a></li>
+          <li><a href="<?php echo get_site_url().'/index.php/copper-kitchen-bathtubs/tiles?category_name=tiles'?>">Tiles</a></li>
+        </ul>
+      </div>
+
+      <div class="col-sm-4">
+        <ul>
+          <li><a href="<?php echo get_site_url().'/index.php/handicrafts/utensils-tableware?category_name=utensils-tableware'?>">Utensils & Tableware</a></li>
+          <li><a href="<?php echo get_site_url().'/index.php/handicrafts/wall-decor?category_name=wall-decor'?>">Wall Decor</a></li>
+          <li><a href="<?php echo get_site_url().'/index.php/handicrafts/furniture?category_name=furniture'?>">Furniture</a></li>
+          <li><a href="<?php echo get_site_url().'/index.php/handicrafts/lamps-lighting?category_name=lamps-lighting'?>">Lamps & Lighting</a></li>
+          <li><a href="<?php echo get_site_url().'/index.php/handicrafts/garden?category_name=garden'?>">Garden</a></li>
+        </ul>
+      </div>
+
+
+    </div>
+  </div>
+</footer>
+<?php
+  wp_footer();
+?>
+<script>
+var lazyLoadInstance = new LazyLoad({
+    elements_selector: ".lazy"
+});
+</script>
+</body>
+</html>
